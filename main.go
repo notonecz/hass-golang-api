149834aa376eca9d5730f7d8a1fb26b84a667dce
notonecz <vojtik.prouza@gmail.com>
2025-12-29 16:45:53 +0100

    - HTTPS Support + com()

--- main.go
+++ main.go
@@ -4,21 +4,38 @@ import (
 	"hass-golang-api/rest"
 	"os"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	godotenv.Load()
+	_ = godotenv.Load()
 	token := os.Getenv("TOKEN")
 
-	home1 := rest.Init("HOME1", token, "homeassistant.local:8123")
+	home1 := rest.Init("HOME1", token, "homeassistant.local:8123", false)
 
-	state, _ := rest.GetState(home1, "sensor.alarm_clock_24f6_temperature")
+	a := app.New()
+	w := a.NewWindow("Garaz")
 
-	print(string(state))
+	w.Resize(fyne.NewSize(800, 600))
 
-	garage, _ := rest.PostService(home1, "input_button", "press", "{\"entity_id\": \"input_button.garaz\"}")
+	w.SetContent(container.NewVBox(
+		widget.NewLabel("Ovládání Garáže!"),
+		widget.NewButton("Ovladac!", func() {
+			_, _ = rest.PostService(home1, "input_button", "press", "{\"entity_id\": \"input_button.garaz\"}")
+		}),
+		widget.NewLabel("Světlo kuchyň!"),
+		widget.NewButton("Zapnout", func() {
+			_, _ = rest.PostService(home1, "light", "turn_on", "{\"entity_id\": \"light.mini\"}")
+		}),
+		widget.NewButton("Vypnout", func() {
+			_, _ = rest.PostService(home1, "light", "turn_off", "{\"entity_id\": \"light.mini\"}")
+		}),
+	))
 
-	print(string(garage))
+	w.ShowAndRun()
 }
--- rest/com.go
+++ rest/com.go
@@ -8,54 +8,56 @@ import (
 )
 
 type IMain struct {
-	id    string
-	token string
-	ip    string
+	id     string
+	token  string
+	secure bool
+	ip     string
 }
 
-func Init(id string, token string, ip string) *IMain {
+func Init(id string, token string, ip string, secure bool) *IMain {
 	return &IMain{
-		id:    id,
-		token: token,
-		ip:    ip,
+		id:     id,
+		token:  token,
+		ip:     ip,
+		secure: secure,
 	}
 }
 
-func comGet(auth *IMain, endpoint string) ([]byte, error) {
-	url := fmt.Sprintf("http://%s/%s", auth.ip, endpoint)
+func getProtocol(secure bool) string {
+	switch secure {
+	case true:
+		return "https"
+	case false:
+		return "http"
+	}
+	return "http"
+}
+
+func comGet(auth *IMain, endpoint string, secure bool) ([]byte, error) {
+
+	url := fmt.Sprintf("%s://%s/%s", getProtocol(secure), auth.ip, endpoint)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("Authorization", "Bearer "+auth.token)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, errClient := client.Do(req)
-	if errClient != nil {
-		return nil, errClient
-	}
-	defer resp.Body.Close()
-
-	body, errBody := io.ReadAll(resp.Body)
-	if errBody != nil {
-		return nil, errBody
-	}
-
-	return body, nil
+	return com(req, auth.token)
 }
 
-func comPost(auth *IMain, endpoint string, payload string) ([]byte, error) {
-	url := fmt.Sprintf("http://%s/%s", auth.ip, endpoint)
+func comPost(auth *IMain, endpoint string, payload string, secure bool) ([]byte, error) {
+	url := fmt.Sprintf("%s://%s/%s", getProtocol(secure), auth.ip, endpoint)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("Authorization", "Bearer "+auth.token)
+	return com(req, auth.token)
+}
+
+func com(req *http.Request, token string) ([]byte, error) {
+	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
@@ -63,7 +65,12 @@ func comPost(auth *IMain, endpoint string, payload string) ([]byte, error) {
 	if errClient != nil {
 		return nil, errClient
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
 
 	body, errBody := io.ReadAll(resp.Body)
 	if errBody != nil {
